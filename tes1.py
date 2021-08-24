d32237cda0294e0957f1414b775054f8a0707c47
Muttaqin <imutt20041998@gmail.com>
2021-08-24 15:22:27 +0700

    double

--- tes1.py
+++ tes1.py
@@ -1,1 +1,14 @@
 # test
+n = 50
+result = []
+for x in range(1, n+1):
+    if x % 3 == 0 and x % 5 == 0:
+        result.append('Frontend\nBackend')
+    elif x%3 == 0 and x%5 !=0:
+        result.append('Frontend')
+    elif x%5 == 0 and x%3 != 0:
+        result.append('Backend')
+    else:
+        result.append(str(x))
+
+print(','.join(result))
